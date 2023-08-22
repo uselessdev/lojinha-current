c6f77fa36089d50801abffc708c7bb0cb2a90686
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-08-21 22:53:56 -0300

    chore: change lang name to portuguese

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className="min-h-[100dvh] bg-gray-50 grid place-items-center">{children}</body>
       <Analytics />
     </html>
