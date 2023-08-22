da45eb153a66b62cdcc840c339913a7e7c88889e
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-08-21 22:52:14 -0300

    chore: add favicons and metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,34 @@
-import './globals.css'
+import '~/globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Lojinha',
-  description: 'E-commerce headless para desenvolvedores.',
-}
+  title: {
+    default: "lojinha",
+    template: "%s | Lojinha",
+  },
+  description:
+    "Uma plataforma de e-commerce headless para desenvolvedores front-end.",
+  icons: [
+    { rel: "apple-touch-icon", url: "./apple-touch-icon.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+  ],
+  manifest: "/site.webmanifest",
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+    { media: "(prefers-color-scheme: light)", color: "#fefefe" },
+  ],
+};
 
 export default function RootLayout({
   children,
