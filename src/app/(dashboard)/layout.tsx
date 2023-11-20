b0c9a98210d5c97a0568262cd55161464d6e4a5f
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-11-20 12:22:05 -0300

    chore: disable things

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,111 +1,112 @@
 "use client";
 
-import {
-  CreateOrganization,
-  OrganizationSwitcher,
-  UserButton,
-  useOrganization,
-  useSession,
-} from "@clerk/nextjs";
-import Link from "next/link";
-import { dark } from "@clerk/themes";
-import { redirect, usePathname } from "next/navigation";
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-import { Logo } from "~/components/logo";
-import { menu } from "./menu";
-import { ThemeToggle } from "~/components/theme-toggle";
-import { useTheme } from "next-themes";
-import { Toaster } from "~/components/ui/toaster";
+// import {
+//   CreateOrganization,
+//   OrganizationSwitcher,
+//   UserButton,
+//   useOrganization,
+//   useSession,
+// } from "@clerk/nextjs";
+// import Link from "next/link";
+// import { dark } from "@clerk/themes";
+import { redirect /* usePathname */ } from "next/navigation";
+// import clsx from "clsx";
+// import { twMerge } from "tailwind-merge";
+// import { Logo } from "~/components/logo";
+// import { menu } from "./menu";
+// import { ThemeToggle } from "~/components/theme-toggle";
+// import { useTheme } from "next-themes";
+// import { Toaster } from "~/components/ui/toaster";
 
-export default function DashboardLayout({
-  children,
-}: {
+export default function DashboardLayout({} // children,
+: {
   children: React.ReactNode;
 }) {
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
 
-  const { organization } = useOrganization();
-  const { isSignedIn, isLoaded } = useSession();
-  const pathname = usePathname();
+  // const { organization } = useOrganization();
+  // const { isSignedIn, isLoaded } = useSession();
+  // const pathname = usePathname();
 
-  if (isLoaded && !isSignedIn) {
-    return redirect("/");
-  }
+  return redirect(`/`);
 
-  if (!organization) {
-    return (
-      <div className="grid h-[100dvh] w-full place-content-center">
-        <CreateOrganization
-          appearance={{ baseTheme: theme === "dark" ? dark : undefined }}
-          afterCreateOrganizationUrl="/settings"
-        />
-      </div>
-    );
-  }
+  // if (isLoaded && !isSignedIn) {
+  //   return redirect("/");
+  // }
 
-  return (
-    <>
-      <main className="grid h-[100dvh] w-full grid-cols-[300px_1fr]">
-        <aside className="grid grid-rows-[80px_1fr]">
-          <header className="flex h-20 flex-col justify-center px-6">
-            <h1 className="text-xs font-semibold">
-              <Link href="/">
-                <Logo className="h-8 w-8" />
-              </Link>
-            </h1>
-          </header>
+  // if (!organization) {
+  //   return (
+  //     <div className="grid h-[100dvh] w-full place-content-center">
+  //       <CreateOrganization
+  //         appearance={{ baseTheme: theme === "dark" ? dark : undefined }}
+  //         afterCreateOrganizationUrl="/settings"
+  //       />
+  //     </div>
+  //   );
+  // }
 
-          <nav className="flex flex-col gap-2 px-4 pb-6">
-            {menu.map((menu) => (
-              <Link
-                key={menu.url}
-                href={menu.url}
-                className={twMerge(
-                  clsx(
-                    "flex items-center gap-2 rounded-md p-2 text-sm font-medium text-foreground/50 hover:bg-foreground/5",
-                    {
-                      "bg-foreground/5 text-foreground dark:bg-foreground/10":
-                        pathname.startsWith(menu.url),
-                    },
-                  ),
-                )}
-              >
-                {menu.icon} {menu.label}
-              </Link>
-            ))}
+  // return (
+  //   <>
+  //     <main className="grid h-[100dvh] w-full grid-cols-[300px_1fr]">
+  //       <aside className="grid grid-rows-[80px_1fr]">
+  //         <header className="flex h-20 flex-col justify-center px-6">
+  //           <h1 className="text-xs font-semibold">
+  //             <Link href="/">
+  //               <Logo className="h-8 w-8" />
+  //             </Link>
+  //           </h1>
+  //         </header>
 
-            <div className="mt-auto px-2">
-              <ThemeToggle />
-            </div>
-          </nav>
-        </aside>
+  //         <nav className="flex flex-col gap-2 px-4 pb-6">
+  //           {menu.map((menu) => (
+  //             <Link
+  //               key={menu.url}
+  //               href={menu.url}
+  //               className={twMerge(
+  //                 clsx(
+  //                   "flex items-center gap-2 rounded-md p-2 text-sm font-medium text-foreground/50 hover:bg-foreground/5",
+  //                   {
+  //                     "bg-foreground/5 text-foreground dark:bg-foreground/10":
+  //                       pathname.startsWith(menu.url),
+  //                   },
+  //                 ),
+  //               )}
+  //             >
+  //               {menu.icon} {menu.label}
+  //             </Link>
+  //           ))}
 
-        <section>
-          <header className="flex h-20 items-center justify-between pr-6">
-            <OrganizationSwitcher
-              hidePersonal
-              appearance={{
-                baseTheme: theme === "dark" ? dark : undefined,
-                elements: {
-                  organizationSwitcherPopoverActionButton__createOrganization: {
-                    display: "none",
-                  },
-                },
-              }}
-            />
-            <UserButton
-              appearance={{
-                baseTheme: theme === "dark" ? dark : undefined,
-              }}
-            />
-          </header>
+  //           <div className="mt-auto px-2">
+  //             <ThemeToggle />
+  //           </div>
+  //         </nav>
+  //       </aside>
 
-          <div className="pb-6 pr-6">{children}</div>
-        </section>
-      </main>
+  //       <section>
+  //         <header className="flex h-20 items-center justify-between pr-6">
+  //           <OrganizationSwitcher
+  //             hidePersonal
+  //             appearance={{
+  //               baseTheme: theme === "dark" ? dark : undefined,
+  //               elements: {
+  //                 organizationSwitcherPopoverActionButton__createOrganization: {
+  //                   display: "none",
+  //                 },
+  //               },
+  //             }}
+  //           />
+  //           <UserButton
+  //             appearance={{
+  //               baseTheme: theme === "dark" ? dark : undefined,
+  //             }}
+  //           />
+  //         </header>
 
-      <Toaster />
-    </>
-  );
+  //         <div className="pb-6 pr-6">{children}</div>
+  //       </section>
+  //     </main>
+
+  //     <Toaster />
+  //   </>
+  // );
 }
